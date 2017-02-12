402728b01569a5f1e793b41023c3b70aa57120e2
Andrew McPherson <mcphandr@amazon.com>
2017-02-12 14:15:52 -0800

    Moved a sprite across the screen.

--- source/index.js
+++ source/index.js
@@ -1,10 +1,31 @@
 var Pixi = require("pixi.js")
+var Afloop = require("afloop")
 
 Pixi.renderer = Pixi.autoDetectRenderer(320, 240)
-Pixi.renderer.backgroundColor = 0x000000
+Pixi.renderer.backgroundColor = 0x444444
 Pixi.renderer.roundPixels = true
 Pixi.render = function(scene) {
     this.renderer.render(scene)
 }
 
 document.body.appendChild(Pixi.renderer.view)
+
+// Create a scene.
+var scene = new Pixi.Container()
+
+// Create a sprite and add it to the scene.
+var sprite = new Pixi.Sprite.fromImage("images/monster.png")
+sprite.anchor.x = 0.5
+sprite.anchor.y = 0.5
+scene.addChild(sprite)
+
+var loop = new Afloop(function(delta) {
+    
+    // Update the sprite.
+    sprite.position.x += 1.4
+    sprite.position.y += 1
+    sprite.rotation += 0.1
+    
+    // Render the scene.
+    Pixi.render(scene)
+})
