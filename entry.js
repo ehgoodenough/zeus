318dc61069ad2e8625f8d407543310c5b945b22e
Andrew McPherson <mcphandr@amazon.com>
2017-02-12 14:13:42 -0800

    Added a packaging process.

--- entry.js
+++ entry.js
@@ -1,15 +1,19 @@
-var Electron = require("electron")
-var Chokidar = require("chokidar")
 var window = undefined
 
+var Yargs = require("yargs")
+var Electron = require("electron")
+var Chokidar = require("chokidar")
+
 Electron.app.on("ready", function() {
-    window = new Electron.BrowserWindow({width: 800, height: 600})
+    window = new Electron.BrowserWindow({width: 16*50, height: 10*50})
     window.loadURL(`file://${__dirname}/source/index.html`)
-    // window.webContents.openDevTools()
     
-    Chokidar.watch("./source").on("all", function() {
-        window.reload()
-    })
+    if(Yargs.argv.mode == "development") {
+        window.webContents.openDevTools({detach: true})
+        Chokidar.watch("./source").on("all", function() {
+            window.reload()
+        })
+    }
     
     window.on("closed", function() {
         window = null
