91a06c3e8cf873760ba2046b94974589e748b84a
Andrew McPherson <ehgoodenough@gmail.com>
2017-02-16 23:35:28 -0800

    Added a build process using codeglue.

--- source/index.js
+++ source/index.js
@@ -14,18 +14,18 @@ document.body.appendChild(Pixi.renderer.view)
 var scene = new Pixi.Container()
 
 // Create a sprite and add it to the scene.
-var sprite = new Pixi.Sprite.fromImage("images/monster.png")
+var sprite = new Pixi.Sprite.fromImage(require("images/monster.png"))
 sprite.anchor.x = 0.5
 sprite.anchor.y = 0.5
 scene.addChild(sprite)
 
 var loop = new Afloop(function(delta) {
-    
+
     // Update the sprite.
     sprite.position.x += 1.4
     sprite.position.y += 1
     sprite.rotation += 0.1
-    
+
     // Render the scene.
     Pixi.render(scene)
 })
