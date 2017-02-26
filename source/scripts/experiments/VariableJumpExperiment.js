ea531926b2bd24247df24e5b435b60a6ab8940c9
Paul M <paulmuren@gmail.com>
2017-02-26 04:11:37 -0800

    Polished and tweaked variable jumps

--- source/scripts/experiments/VariableJumpExperiment.js
+++ source/scripts/experiments/VariableJumpExperiment.js
@@ -12,9 +12,9 @@ export default class ActionExperiment extends Experiment {
         this.addChild(new Hero())
     }
     get description() {
-        return "... "
-             + "... "
-             + "...!"
+        return "A sprite that has a notion of velocity and acceleration. "
+             + "It can jump to a multitude of altitudes, drift while airborne, "
+             + "and make slight adjustments to its position while grounded."
     }
 }
 
@@ -25,28 +25,62 @@ class Hero extends Sprite {
         super(HERO_TEXTURE)
 
         this.position.x = 320 / 4
-        this.position.y = 180 - 32
+        this.position.y = 0
 
         this.velocity = new Pixi.Point()
 
-        this.speed = 2
+        this.groundSpeed = 2.8
+        this.groundAcceleration = 0.4
+        this.airSpeed = 2.8
+        this.aerialAcceleration = 0.3
 
         this.scale.x *= -1
 
-        this.gravity = 0.5
-        this.gravityDampener = 0.32
-        this.gravityDampeningThreshold = -5
-        this.jumpForce = -6.5
+        this.gravity = 0.65
+        this.gravityDampener = 0.3
+        this.gravityDampeningThreshold = -3.5
+        this.jumpForce = -5.8
+        this.minJumpHeight = 20
+        this.groundedYPosition = 180 - 36
     }
     update(delta) {
+        var applyGroundedFriction = true
         if(Keyb.isDown("A") || Keyb.isDown("<left>")) {
-            this.velocity.x = -1 * this.speed
-            this.scale.x = +1
+            if(this.isGrounded()) {
+                this.scale.x = +1
+                if(this.velocity.x > -1 * this.groundSpeed) {
+                    this.velocity.x -= this.groundAcceleration
+                    if(this.velocity.x < -1 * this.groundSpeed) {
+                        this.velocity.x = -1 * this.groundSpeed
+                    }
+                    applyGroundedFriction = false
+                }
+            } else if(this.velocity.x > -1 * this.airSpeed) {
+                this.velocity.x -= this.aerialAcceleration
+                if(this.velocity.x < -1* this.airSpeed){
+                    this.velocity.x = -1 * this.airSpeed
+                }
+            }
         }
 
         if(Keyb.isDown("D") || Keyb.isDown("<right>")) {
-            this.velocity.x = +1 * this.speed
-            this.scale.x = -1
+            if(this.isGrounded()) {
+                this.scale.x = -1
+                if(this.velocity.x < +1 * this.groundSpeed) {
+                    this.velocity.x += this.groundAcceleration
+                    if(this.velocity.x > +1 * this.groundSpeed) {
+                        this.velocity.x = +1 * this.groundSpeed
+                    }
+                    applyGroundedFriction = false
+                }
+            } else {
+                if(this.velocity.x < this.airSpeed) {
+                    this.velocity.x += this.aerialAcceleration
+                    if(this.velocity.x > +1 * this.airSpeed){
+                        this.velocity.x = +1 * this.airSpeed
+                    }
+                }
+            }
         }
 
         if(Keyb.isDown("W") || Keyb.isDown("<up>")) {
@@ -58,19 +92,26 @@ class Hero extends Sprite {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        this.velocity.x *= 0.8
+        //Horizontal Friction
+        if(this.isGrounded() && applyGroundedFriction) {
+            this.velocity.x *= 0.68
+        }
 
         //Gravity
         if(!this.isGrounded()) {
-            //console.log(this.velocity.y < this.gravityDampeningThreshold)
-            if(this.velocity.y < this.gravityDampeningThreshold && (Keyb.isDown("W") || Keyb.isDown("<up>"))) {
+            if(this.velocity.y < this.gravityDampeningThreshold &&
+                (Keyb.isDown("W") || Keyb.isDown("<up>")) ||
+                this.groundedYPosition - this.position.y < this.minJumpHeight) {
+                //If anything else ever causes the character to move upward
+                //We may need to make sure that this gravity dampening
+                //Only happens during a jump action
                 this.velocity.y += this.gravity * this.gravityDampener * delta.f
             } else {
                 this.velocity.y += this.gravity * delta.f
             }
         } else {
             this.velocity.y = 0
-            this.position.y = 180-32
+            this.position.y = this.groundedYPosition
         }
 
         if(this.position.x < 0) {
@@ -80,7 +121,7 @@ class Hero extends Sprite {
         }
     }
     isGrounded() {
-        if(this.position.y >= 180 - 32) {
+        if(this.position.y >= this.groundedYPosition) {
             return true
         } else {
             return false
