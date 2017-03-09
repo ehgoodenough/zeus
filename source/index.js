ffa35d80c58ed4cb5e00b1cfd528cc2a68c36c79
Andrew McPherson <ehgoodenough@gmail.com>
2017-03-09 12:16:53 -0800

    Re-added the FPS Meter; I forgot we had that!!

--- source/index.js
+++ source/index.js
@@ -1,5 +1,6 @@
 import * as Pixi from "pixi.js"
 import Yaafloop from "yaafloop"
+import FPSmeter from "fpsmeter"
 import "statgrab/do"
 
 ///////////////
@@ -14,10 +15,29 @@ var frame = document.getElementById("frame")
 frame.appendChild(game.renderer.view)
 
 ////////////////////
+// The FPS Meter //
+//////////////////
+
+import DevMode from "scripts/layers/DevMode.js"
+
+var meter = undefined
+if(DevMode.isActive) {
+    meter = new FPSMeter(frame, {
+        theme: "colorful", graph: true, heat: true,
+        left: "auto", top: "10px", right: "10px",
+        decimals: 0,
+    })
+}
+
+////////////////////
 // The Main Loop //
 //////////////////
 
 var loop = new Yaafloop(function(delta) {
     game.update(delta)
     game.render()
+
+    if(!!meter) {
+        meter.tick()
+    }
 })
